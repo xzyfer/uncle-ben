9e7d8189d9e98f29bbae688cfc4e66ddc62cca00
xzyfer <xzyfer@gmail.com>
2012-05-28 02:03:51 +1000

    Get .har contents for a given url

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+;
 
 var app = module.exports = express.createServer();
 
@@ -29,7 +30,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/get', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@
  * GET home page.
  */
 
+var shell = require('shelljs')
+  , pretty = require('pretty-data').pd
+;
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	var url = req.param('url');
+	var cmd = 'phantomjs ~/netsniff.js "' + url + '"';
+	var result = shell.exec(cmd, {silent:true}).output;
+
+	res.render('index', { title: 'Express', url: url, result: result });
 };
