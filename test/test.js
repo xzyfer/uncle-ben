84b628b4d01d9651328d7d838545bbaa3f9699fc
xzyfer <xzyfer@gmail.com>
2012-05-31 14:20:57 +1000

    Add basic controller test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,65 @@
-var request = require('./support/http')
-  , app = require('../app')
+var profiles = require('../controllers/profile')
+  // , sinon = require('sinon')
+  , sandbox = require('sandboxed-module')
   , should = require('should')
 ;
 
-describe('get', function() {
-	describe('GET /get?url=http://example.com', function() {
-		it('should return 200 code', function(done) {
-			request(app)
-				.request('GET', '/get?url=http://google.com')
-				.expect(200, done);
-		})
+describe('GET /profiles?url=http://example.com', function() {
+	it('should return 200 code', function(done) {
+
+		var json = {output: '{"foo":"bar","bar":"baz"}'};
+
+		var request = {
+			param: function(k) {
+				switch (k) {
+					case 'url':
+						return 'http://example.com';
+						break;
+				}
+				return;
+			},
+			connectToDb: function() {}
+		};
+
+		var response = {
+			viewName: ""
+			, data : {}
+			, foo: JSON.parse('{"foo":"bar","bar":"baz"}')
+			, render: function(view, viewData) {
+				this.viewName = view;
+				this.data = viewData;
+			}
+		};
+
+		var profiles = sandbox.require('../controllers/profile', {
+			requires: {
+				'shelljs': {
+					exec: function(cmd) {
+						console.log('--------------');
+						return json;
+					}
+				},
+				'../models/profiles': {
+					model: function() {
+						return {
+							save: function() {}
+						}
+					}
+				}
+			}
+		});
+
+		profiles.create(request, response, function() {
+			response.viewName.should.equal('index');
+			response.should.be.a('object');
+			response.should.have.property('data');
+
+			// response.data.should.be.a('object');
+			// response.data.should.have.property('result');
+			// response.data.result.should.equal(JSON.parse(json.output));
+			done();
+		});
+
 	})
 })
 ;
