282a013511ac184e4262ffd42156a4d4ab607162
xzyfer <xzyfer@gmail.com>
2012-06-05 14:21:54 +1000

    Add response cookies to profile

--- helpers/netsniff.js
+++ helpers/netsniff.js
@@ -32,7 +32,7 @@ function createHAR(address, title, timings, resources)
                 method: request.method,
                 url: request.url,
                 httpVersion: "HTTP/1.1",
-                cookies: [],
+                cookies: request.cookies || [],
                 headers: request.headers,
                 queryString: [],
                 headersSize: -1,
@@ -42,7 +42,7 @@ function createHAR(address, title, timings, resources)
                 status: endReply.status,
                 statusText: endReply.statusText,
                 httpVersion: "HTTP/1.1",
-                cookies: [],
+                cookies: endReply.cookies || [],
                 headers: endReply.headers,
                 redirectURL: "",
                 headersSize: -1,
@@ -62,6 +62,8 @@ function createHAR(address, title, timings, resources)
                 receive: endReply.time - startReply.time,
                 ssl: -1
             }
+            // rawRequest: request,
+            // rawResponse: endReply
         });
     });
 
@@ -95,7 +97,41 @@ function evaluate(page, func) {
     return page.evaluate(fn);
 }
 
+// take from the node module cookie.js
+function parseCookie(str) {
+    var obj = {}
+    var pairs = str.split(/[;,] */);
+
+    pairs.forEach(function(pair) {
+        var eq_idx = pair.indexOf('=')
+        var key = pair.substr(0, eq_idx).trim()
+        var val = pair.substr(++eq_idx, pair.length).trim();
+
+        // quoted values
+        if ('"' == val[0]) {
+            val = val.slice(1, -1);
+        }
+
+        // only assign once
+        if (undefined == obj[key]) {
+            val = val.replace(/\+/g, ' ');
+            try {
+                obj[key] = decodeURIComponent(val);
+            } catch (err) {
+                if (err instanceof URIError) {
+                    obj[key] = val;
+                } else {
+                    throw err;
+                }
+            }
+        }
+    });
+
+    return obj;
+}
+
 const PHANTOM_FUNCTION_PREFIX = '/* PHANTOM_FUNCTION */';
+const PHANTOM_ARG_SEPARATOR = '/*+*/'; // obscure enough?
 
 var page = require('webpage').create(),
     system = require('system'),
@@ -127,6 +163,14 @@ if (system.args.length === 1) {
         }
         if (res.stage === 'end') {
             page.resources[res.id].endReply = res;
+
+            // use page evaluate when upgraded to phantomjs 1.6
+            // source: https://gist.github.com/2475509/84e42ef8ce630680e373510f808f958354bf98f2
+            evaluate(page, function(cookieMsg, separator, resId) {
+                if(document.location.href !== 'about:blank') {
+                    console.log([cookieMsg, document.cookie, resId].join(separator));
+                }
+            }, PHANTOM_FUNCTION_PREFIX + page.onCookie, PHANTOM_ARG_SEPARATOR, res.id);
         }
     };
 
@@ -142,10 +186,22 @@ if (system.args.length === 1) {
 
     page.onConsoleMessage = function(msg) {
         if (msg.indexOf(PHANTOM_FUNCTION_PREFIX) === 0) {
-            eval('(' + msg + ')()');
+            var parts = msg.split(PHANTOM_ARG_SEPARATOR);
+            var args;
+
+            // TODO: handle escapeing of " within arg strings
+            if(parts.length > 1) {
+                args = '"' + parts.slice(1).join('","') + '"';
+            }
+
+            eval('(' + parts[0] + ')(' + args + ')');
         } else {
             console.log(msg);
         }
+    };
+
+    page.onCookie = function(str, resId) {
+        page.resources[resId].endReply.cookies = parseCookie(str);
     };
 
     page.onDOMContentLoaded = function() {
