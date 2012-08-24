c51a7124b3d87cfe1b8e9b063fce74db152612f5
xzyfer <xzyfer@gmail.com>
2012-08-24 15:59:17 +1000

    Allow node to cluster over all available cores

--- app.js
+++ app.js
@@ -10,15 +10,23 @@ if(!process.env.NODE_ENV) process.env.NODE_ENV="local"
 
 //  Load boot file and fire away!
 
-var app = require('./config/app')();
-var port = process.env.PORT || 3000;
+var cluster = require('cluster')
+  , numCPUs = require('os').cpus().length;
 
-app.listen(port);
+if ( cluster.isMaster ) {
+  for ( var i=0; i<numCPUs; ++i )
+    cluster.fork();
+} else {
+    var app = require('./config/app')();
+    var port = process.env.PORT || 3000;
 
-console.log('\x1b[36mUncle Ben\x1b[90m v%s\x1b[0m running as \x1b[1m%s\x1b[0m on http://%s:%d in %s region',
-  app.set('version'),
-  app.set('env'),
-  app.set('host'),
-  app.address().port,
-  process.env.REGION || 'us'
-);
+    app.listen(port);
+
+    console.log('\x1b[36mUncle Ben\x1b[90m v%s\x1b[0m running as \x1b[1m%s\x1b[0m on http://%s:%d in %s region',
+        app.set('version'),
+        app.set('env'),
+        app.set('host'),
+        port,
+        process.env.REGION || 'us'
+    );
+}
